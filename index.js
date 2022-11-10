691585bfbb40c9f18564111e551e3e8e740bfca1
salohiddin1999 <salohiddinsaydinov@gmail.com>
2022-11-10 21:27:00 +0500

    misollar

---
+++ index.js
@@ -0,0 +1,77 @@
+// Tiplar bolimi
+// 1)
+// var L = prompt('son kiritng:');
+// var X = L / 100;
+// alert (`natija: ${L} sonida ${X} ta metr bor`);
+
+// 2)
+// var M = prompt('son kiritng:');
+// var X = M / 1000;
+// alert (`natija: ${M} sonida ${X} ta tonna bor`);
+
+// 3)
+// var A = prompt('son kiriting:');
+// var B = prompt('son kiriting:');
+// var X = A > B ;
+// var yechim = A / B ;
+// alert(`natija: ${A} kesmada ${yechim} marta B kesmani joylashtirish mumkin`);
+
+// 4)
+// var XY = prompt('ikki xonali son kiriting:');
+// var Y = XY % 10;
+// var X = (XY - Y)/10;
+
+// if(9 < XY < 100){
+// alert('ikki xonali sonning birlik xonasi raqami '+ Y +' onliklar xonasidagi raqami '+ X );
+// }else if(0 <= XY <= 9){
+//   alert('siz bir xonali son kiritdingiz');
+// }
+// else{
+//     alert('xato malumot kiritdingiz ');
+// }
+
+// Mantiqiy amallar
+
+// 1)
+// var A = prompt('son kiriting:');
+// if (A > 0) {
+//   alert(A + ' musbat son');
+// }
+// else if(A == 0){
+//   alert(A + ' nol');
+// } else if(A < 0) {
+//   alert(A + ' manfiy son');
+// } else {
+//   alert('xato malumot kiritdingiz.');
+// }
+
+// 2 va 3)
+// var A = prompt ('son kiriting:');
+// if (A % 2 != 0) {
+//   alert(A + ' soni toq son');
+// } else if (A % 2 == 0) {
+//   alert(A + ' soni juft son');
+// } else {
+//   alert('xato malumot kiritdingiz');
+// }
+
+// 10)
+// var A = prompt('son kiriting');
+
+// if (A == 1) {
+//   alert('Dushanba');
+// }else if (A == 2) {
+//   alert('Seshanba');
+// }else if (A == 3) {
+//   alert('Chorshanba');
+// }else if (A == 4) {
+//   alert('Payshanba');
+// }else if (A == 5) {
+//   alert('Juma');
+// }else if (A == 6) {
+//   alert('Shanba');
+// }else if (A == 7) {
+//   alert('Yakshanba');
+// } else {
+//   alert('bu raqam uchun hafta kuni mavjud emas.');
+// }
